cae436985afa6b6564ea4d94cc99657221e895a5
sk <s.k.recode226@gmail.com>
2022-11-29 18:12:00 +0900

    first commit

---
+++ chapter1/chapter1/D3D12AppBase.cpp
@@ -0,0 +1,25 @@
+#include "D3D12AppBase.h"
+
+D3D12AppBase::D3D12AppBase()
+{
+}
+
+D3D12AppBase::~D3D12AppBase()
+{
+}
+
+void D3D12AppBase::Initialize(HWND hwnd)
+{
+}
+
+void D3D12AppBase::Terminate()
+{
+}
+
+void D3D12AppBase::Render()
+{
+}
+
+void D3D12AppBase::PrepareDescriptorHeaps()
+{
+}
